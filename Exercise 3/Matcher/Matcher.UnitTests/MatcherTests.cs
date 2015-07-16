c9635e21135f7e2c999a1648340eb73a42a89417
Chris Ross <chris.ross@7digital.com>
2015-07-16 16:43:48 +0100

    first commit

---
+++ Exercise 3/Matcher/Matcher.UnitTests/MatcherTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Matcher.UnitTests
+{
+	[TestFixture]
+	public class MatcherTests
+	{
+		[Test]
+		public void TestMatch()
+		{
+			Matcher matcher = new Matcher();
+			int[] expected = new int[] { 10, 50, 30, 98 };
+			int clipLimit = 100;
+			int delta = 5;
+
+			int[] actual = new int[] { 12, 55, 25, 110 };
+			Assert.IsTrue(matcher.Match(expected, actual, clipLimit, delta));
+			actual = new int[] { 10, 60, 30, 98 };
+			Assert.IsTrue(!matcher.Match(expected, actual, clipLimit, delta));
+			actual = new int[] { 10, 50, 30 };
+			Assert.IsTrue(!matcher.Match(expected, actual, clipLimit, delta));
+		}
+	}
+}
---
+++ Exercise 3/Matcher/Matcher/Matcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Matcher
+{
+	public class Matcher
+	{
+		public bool Match(int[] expected, int[] actual, int clipLimit, int delta)
+		{
+			// Clip "too-large" values
+			for (int i = 0; i < actual.Length; i++)
+				if (actual[i] > clipLimit)
+					actual[i] = clipLimit;
+
+			// Check for length differences
+			if (actual.Length != expected.Length)
+				return false;
+
+			// Check that each entry within expected +/- delta
+			for (int i = 0; i < actual.Length; i++)
+				if (Math.Abs(expected[i] - actual[i]) > delta)
+					return false;
+
+			return true;
+		}
+	}
+}
